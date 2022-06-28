42551f5cf02a77407590be80a03901df315e26af
ElyseRH <elyserhoran@gmail.com>
2022-06-28 11:29:04 +0000

    Rename products to standard_products and add urls.py files

--- w_shop/settings.py
+++ w_shop/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'landing_page',
-    'products',
+    'standard_products',
     'custom_products',
     'loot',
     'checkout',
--- w_shop/urls.py
+++ w_shop/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('landing_page.urls')),
+    # path('standard_products/', include('standard_products.urls')),
+    # path('custom_products/', include('custom_products.urls')),
+    # path('checkout/', include('checkout.urls')),
+    # path('loot/', include('loot.urls')),
+    # path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
