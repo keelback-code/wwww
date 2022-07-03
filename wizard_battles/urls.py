662d8b19e0f830334bcab2975185d0c53898c1a8
ElyseRH <elyserhoran@gmail.com>
2022-07-03 13:11:12 +0000

    Style privacy policy page

--- wizard_battles/urls.py
+++ wizard_battles/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('wizard_battle/', views.view_battle, name='view_battle'),  # change to <slug:battle>/
+    path('wizard_battle/', views.view_battle, name='view_battle'),  # change to <slug:wizard_battle>/
+    path('battle_arena/', views.battle_arena, name='battle_arena'),
     # path('<int:product_id>/', views.product_detail, name='product_detail'),
     # path('add/', views.add_product, name='add_product'),
     # path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
--- wizard_battles/views.py
+++ wizard_battles/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+def battle_arena(request):
+    """
+    Function to retrieve a view of all wizard battle blog posts.
+    """
+    return render(request, 'wizard_battles/battle_arena.html')
+
+
 def view_battle(request):
     """
-    Function to retrieve wizard battle blog posts.
+    Function to retrieve individual wizard battle blog posts.
     """
     return render(request, 'wizard_battles/wizard_battle.html')
