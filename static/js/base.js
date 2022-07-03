44625bb6193a5567cbc58445a22ae2bd8d639b7c
ElyseRH <elyserhoran@gmail.com>
2022-07-03 13:51:55 +0000

    Add back to top button

---
+++ static/js/base.js
@@ -0,0 +1,23 @@
+// Code for back to top button on clothing tips page
+// from W3 https://www.w3schools.com/howto/howto_js_scroll_to_top.asp 
+
+mybutton = document.getElementById( "backBtn" );
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction ()
+{
+  if ( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 )
+  {
+    mybutton.style.display = "block";
+  } else
+  {
+    mybutton.style.display = "none";
+  }
+};
+
+function topFunction ()
+{
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
