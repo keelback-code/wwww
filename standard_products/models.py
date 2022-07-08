92a506e449fdf0ed46eac7109420c06ef932de3f
ElyseRH <elyserhoran@gmail.com>
2022-07-08 14:52:32 +0000

    Add standard product model

--- standard_products/models.py
+++ standard_products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class StandardProduct(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    stock_level = models.IntegerField(default=0)
+    stat_category_one = models.CharField(max_length=254, null=True, blank=True)
+    stat_number_one = models.IntegerField(null=True, blank=True)
+    stat_category_two = models.CharField(max_length=254, null=True, blank=True)
+    stat_number_two = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
