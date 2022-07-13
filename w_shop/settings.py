3b5bdb44ca75c6bb54808c4571ce533b530ab0a7
ElyseRH <elyserhoran@gmail.com>
2022-07-13 11:16:34 +0000

    Change to bootstrap 4 for toasts to work

--- w_shop/settings.py
+++ w_shop/settings.py
@@ -62,7 +62,7 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-CRISPY_TEMPLATES_PACK = 'bootstrap5'
+CRISPY_TEMPLATES_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
