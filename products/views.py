387650d79a87006491b8d87f9224148473eab94f
ElyseRH <elyserhoran@gmail.com>
2022-07-18 13:11:43 +0000

    Add html and views for custom cloak and wand

--- products/views.py
+++ products/views.py
@@ -135,6 +135,43 @@ class DesignCustomHatTwo(View):
         return render(request, template, context)
 
 
+class DesignCustomCloak(View):
+    """
+    Class to get a quote for a custom cloak.
+    """
+    def get(self, request):
+        form = CloakForm()
+        template = 'products/custom_cloak.html'
+        context = {
+            'form': form,
+        }
+
+        return render(request, template, context)
+
+
+    def post(self, request):
+        form = CloakForm(request.POST) 
+        cloak_length = request.POST['variable_one']
+        cloak_pattern = request.POST['variable_two']
+        clasp_choices = request.POST['variable_three']
+        
+        if form.is_valid():
+            priced_form = form.save(commit=False)
+            priced_form.price = calc_variables(cloak_length, cloak_pattern, clasp_choices)
+            product = form.save()
+            return redirect(reverse('final_quote', args=[product.id]))
+        else:
+            messages.error(request, 'Quote was not generated. Please try again.')
+            form = CloakForm()
+
+        template = 'products/custom_cloak.html'
+        context = {
+            'form': form,
+        }
+
+        return render(request, template, context)
+
+
 def final_quote(request, product_id):
     """
     Function to display a quote for a custom product.
