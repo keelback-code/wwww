8ce2dd960581cb75f170f03a62dfe4904a110d0b
ElyseRH <elyserhoran@gmail.com>
2022-07-27 15:00:13 +0000

    Add landing image for deployment test

--- w_shop/settings.py
+++ w_shop/settings.py
@@ -233,9 +233,12 @@ STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_USE_TLS = True
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_USE_TLS = True
+# EMAIL_PORT = os.environ.get('EMAIL_PORT')
+# EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
