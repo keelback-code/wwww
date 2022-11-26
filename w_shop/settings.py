19d8d88c92a01c98528d075cdba8a6df20844930
ElyseRH <elyserhoran@gmail.com>
2022-11-26 14:42:28 +0000

    Add variables for render platform

--- w_shop/settings.py
+++ w_shop/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['w-w-w-w.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['w-w-w-w.onrender.com', 'localhost']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
